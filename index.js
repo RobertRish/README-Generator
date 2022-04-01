011ef3d6d9fdc2aef1d93b39f3771717f5d6c99a
Robby Rish <robbyrish13@gmail.com>
2022-04-01 01:18:14 -0400

    fixed file tree

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateREADME = require('./utils/generateREADME.js')
-const generateLicense = require('./utils/generateMarkdown.js');
+const generateREADME = require('./develop/utils/generateREADME.js')
+const generateMarkdown = require('./develop/utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const promptProject = portfolioData => {
